eb162234ee154ef260effc2615865b6d16b08211
Mariia Tyshchenko <m.tish2406@gmail.com>
2023-07-03 13:59:54 +0300

    add style file

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -4,8 +4,9 @@ const url = 'https://api.thecatapi.com/v1/breeds';
 axios.defaults.headers.common["x-api-key"] = "live_RDzJkzURGbmWQIQ946VNcb7AS8SyxbJd4RjkgNCLGaz1JwIxIF2heBTvfgbSFZSU";
 let storedBreeds = [];
 
+
 export const fetchBreeds = catsData => {
-    fetch(`${url}?q=`)
+    fetch(`${url}?api_key=${axios.defaults.headers.common["x-api-key"]}&breed=${catsData}`)
         .then(response => {
             if (!response.ok) {
                 throw new Error(response.status);
@@ -13,27 +14,39 @@ export const fetchBreeds = catsData => {
             return response.json();
         })
         .then(catsData => {
-            console.log(catsData);
             storedBreeds = catsData;
-            console.log(storedBreeds);
-        })
+            for (let i = 0; i < storedBreeds.length; i++) {
+    const breed = storedBreeds[i];
+    let option = document.createElement('option');
+   
+    //use the current array index
+    option.value = i;
+    option.innerHTML = `${breed.name}`;
+document.getElementById('breed_selector').appendChild(option);
+    
+    }
+   //show the first breed by default
+   showBreedImage(0)
+})
+        
         .catch(error => {
             console.log(error);
         });
 }
 
-// fetch(`${url}`)
-//   .then(response => { 
-//     if (!response.ok) {
-//       throw new Error(response.status);
-//     }
-//     return response.json();
-//   })
-//   .then(catsData => {
-//     console.log(catsData);
-//     storedBreeds = catsData;
-//     console.log(storedBreeds);
-//     })
-//     .catch(error => {
-//         console.log(error);
-//     });
+function showBreedImage(index)
+{ 
+  const breedImg = document.createElement('img');
+  breedImg.src= storedBreeds[index].image.url;
+  document.querySelector('.cat-info').append(breedImg);
+ 
+  const breedDescr = `<div class='breed-descr'><h1 class='breed-title'>${storedBreeds[index].name}</h1><p>${storedBreeds[index].description}</p><p><b>Temperament:</b> ${storedBreeds[index].temperament}</p></div>`;
+
+document.querySelector('.cat-info').insertAdjacentHTML("beforeend", breedDescr);
+  
+  // document.getElementById("breed_json").textContent= storedBreeds[index].temperament
+  // document.getElementById("breed_json").textContent= storedBreeds[index].description
+  
+
+}
+
